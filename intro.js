e0b31e02a99778b156a63d73b5c52506c733e658
Arunsamuvel <arun15ee03@gmail.com>
2024-09-30 12:40:00 +0530

    global setup

---
+++ intro.js
@@ -0,0 +1,7 @@
+// globals- no windows
+
+// _dirname - path to current directory
+// _filename- file name
+// require-function to use modules(CommonJS)
+// module - info about current module
+//  process - info  about env where the program is being executed
